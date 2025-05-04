d307566039dee08162771e42e0b91947e44ddfaf
T S Ajeet <ajeet.tumuluru@gmail.com>
2025-05-04 15:53:24 +0530

    exit 0

--- app/main.go
+++ app/main.go
@@ -3,19 +3,39 @@ package main
 import (
 	"bufio"
 	"fmt"
-	"log"
 	"os"
+	"strconv"
+	"strings"
 )
+
+const EXIT = "exit"
 
 func main() {
 	for {
 		fmt.Fprint(os.Stdout, "$ ")
 
-		command, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		inputCommand, err := bufio.NewReader(os.Stdin).ReadString('\n')
 		if err != nil {
-			log.Fatalf("Unable to read input command: %s", command)
+			fmt.Printf("Unable to read input command: %s\n", inputCommand)
 		}
 
-		fmt.Printf("%s: command not found\n", command[:len(command)-1])
+		inputCommandParts := strings.Fields(inputCommand)
+		command, args := inputCommandParts[0], inputCommandParts[1:]
+
+		switch command {
+		case EXIT:
+			if len(args) < 1 {
+				fmt.Printf("Exit code required as an arg: %v\n", args)
+				break
+			}
+			exitCode, err := strconv.Atoi(args[0])
+			if err != nil {
+				fmt.Printf("Error converting string to number: %v\n", err)
+				break
+			}
+			os.Exit(exitCode)
+		default:
+			fmt.Printf("%s: command not found\n", command)
+		}
 	}
 }
